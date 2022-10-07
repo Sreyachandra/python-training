9d1125bc68cafbbf77dbf399f30d9ceb092ad0c1
sreyachandra <chinnusreyachandran@gmail.com>
2022-10-07 20:00:13 +0530

    added tuple.py

---
+++ tuple.py
@@ -0,0 +1,20 @@
+#creating tuple
+a = ("apple","orange")
+print(a)                                               #('apple', 'orange')
+a = tuple((“apple”,
+#length of tuple
+a = tuple(("apple","orange"))
+print(len(a))                                          #2
+tuple1 = ("apple","orange")
+tuple2 = (1,6,7)
+tuple3 = (True,False)
+print(tuple1)
+print(tuple2)
+print(tuple3)                                           #('apple', 'orange')
+                                                                 (1, 6, 7)
+                                                                (True, False)
+#tuple contain different data types
+a= ("apple",30,"orange",True)
+print(a)                                                #      ('apple', 30, 'orange', True)
+a= (5,8,'sre')
+print(len(a))                                           #3
